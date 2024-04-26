e782616896ae06a6e25f3a2e487b4e690522a803
OscarZ <121022918+osczapmar@users.noreply.github.com>
2024-04-26 10:19:10 +0200

    Update index.js

--- Web/robot-escape/src/pages/index.js
+++ Web/robot-escape/src/pages/index.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+            <Link legacyBehavior href="/about"> 
+                <a>About</a>
+            </Link>
+        </div>
+    );
+}

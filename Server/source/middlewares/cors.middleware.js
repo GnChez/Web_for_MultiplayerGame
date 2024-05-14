26e1d9d07b10f8cec88b9b6a2047612204bdd75f
osczapmar <a22osczapmar@inspedralbes.cat>
2024-05-14 14:41:36 +0200

    cors corrected

--- Server/source/middlewares/cors.middleware.js
+++ Server/source/middlewares/cors.middleware.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: "http://robot-escape.dam.inspedralbes.cat:3000",
+    origin: "http://robot-escape.dam.inspedralbes.cat",
     credentials: true,
     methods: ['GET', 'POST', 'PUT','DELETE'],
     exposedHeaders: ['set-cookie', 'ajax-redirect'],
